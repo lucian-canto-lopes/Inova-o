18b94e1694ed86cec42a1e39d13d0bf9dd083048
zzrispu <jarturoliveira30@gmail.com>
2025-10-17 10:25:55 -0300

    Filtro para CardDeck

--- src/components/CardDeck.tsx
+++ src/components/CardDeck.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import {  FaEye, FaEyeSlash, FaFilter } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaFilter } from 'react-icons/fa';
 import { DimensaoEnum, DimensaoTipo } from './Modal';
 import '../css/CardDeck.css';
 import { BiSort } from 'react-icons/bi';
@@ -13,30 +13,75 @@ interface Props {
   onCardClick: (dimensao: any) => void;
 }
 
+interface DimensoesFilter {
+  disciplinas: true,
+  eventos: true,
+  motores: true,
+  negocios: true,
+}
+
+const sortArray: string[] = ["Data", "Cres", "Decr"];
+
 function CardDeck({ title, hasFilter = false, data, onCardClick }: Props) {
   const [isFilterOpen, setFilterOpen] = useState<boolean>(false);
   const dimensoesTipos = Object.keys(DimensaoEnum) as DimensaoTipo[];
+
+  const [sortIndex, setSortIndex] = useState<number>(1);
+  const [dimensoesFilter, setDimensoesFilter] = useState<DimensoesFilter>({
+    disciplinas: true,
+    eventos: true,
+    motores: true,
+    negocios: true,
+  });
+
+  const filteredData = data.filter((item: {tipo: DimensaoTipo}) => dimensoesFilter[item.tipo]);
 
   return (
     <section className="CardDeck">
       <header>
         <h2>{title}</h2>
-        {hasFilter ? <FaFilter size={'1.25em'} onClick={() => setFilterOpen(!isFilterOpen)}/> : ''}
+        {hasFilter ? <FaFilter size={'1.25em'} onClick={() => setFilterOpen(!isFilterOpen)} /> : ''}
         {isFilterOpen && (
           <div className="filter-box">
-            <div><span><BiSort />Ordenar por</span><span className='filter-sort'>Data</span></div>
+            <div onClick={() => setSortIndex(prev => {
+              const newIndex = (prev + 1) % sortArray.length;
+
+              switch (newIndex) {
+                case 0:
+                  data.sort((a: any, b: any) => b.id - a.id);
+                  break;
+                case 1:
+                  data.sort((a: any, b: any) => a.data.nome.localeCompare(b.data.nome));
+                  break;
+                case 2:
+                  data.sort((a: any, b: any) => b.data.nome.localeCompare(a.data.nome));
+                  break;
+                default:
+                  break;
+              }
+              return newIndex;
+
+            })}><span><BiSort />Ordenar por</span><span className='filter-sort'>{sortArray[sortIndex]}</span></div>
             <hr />
             {dimensoesTipos.map(dimensao => {
               const qtd = data.reduce((acc: number, cur: any) => cur.tipo === dimensao ? ++acc : acc, 0);
-              return <div key={`span-${dimensao}`}><span><FaEye />{DimensaoEnum[dimensao]}</span><span>{qtd}</span></div>
+              return <div key={`span-${dimensao}`} onClick={() => {
+                // console.log(data.filter((obj: any) => obj.tipo != dimensao));
+                setDimensoesFilter((prev: any) => ({
+                  ...prev,
+                  [dimensao]: !prev[dimensao],
+                }));
+              }}><span>{dimensoesFilter[dimensao] ? <FaEye /> : <FaEyeSlash />}{DimensaoEnum[dimensao]}</span><span>{qtd}</span></div>
+
             })}
           </div>
         )}
       </header>
       <div className="deck-cards">
-        {data.map((dimensao: any) => {
+        {filteredData.map((dimensao: any) => {
           const n = Math.round(Math.random() * 10);
-          return <Card key={`${n}-${dimensao.data.dimensaoId}`} data={dimensao} onClick={onCardClick}/>
+          return <Card key={`${n}-${dimensao.data.dimensaoId}`} data={dimensao} onClick={onCardClick} />
+
         })}
       </div>
     </section>
