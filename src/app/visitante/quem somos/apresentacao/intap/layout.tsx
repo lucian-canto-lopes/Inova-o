c8a5257e240090250a89e027396cc6bd1e7ac96f
Lucian-Canto-lopes <lucianlopes7@gmail.com>
2025-10-19 20:15:09 -0300

    criação de apresentações

---
+++ src/app/visitante/quem somos/apresentacao/intap/layout.tsx
@@ -0,0 +1,10 @@
+// filepath: c:\meus codigos\piape 2\src\app\layout.tsx
+import ".../globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
